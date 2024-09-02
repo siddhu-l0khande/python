797fe483e9036a8ce5a8a0336bd43d4598440487
siddhant lokhande <lokhandesiddharth196@gmail.com>
2024-09-02 20:08:59 +0530

    include

---
+++ python basics/sec.py
@@ -0,0 +1,8 @@
+print("HELLO\nWorld")
+
+#this the code in python 
+
+'''happy to type code in python 
+and starting the journey with python 
+i feeled this is more easy and simple language  than 
+any language '''
---
+++ python basics/variable.py
@@ -0,0 +1,8 @@
+x="system"
+y=756
+z=90.73
+
+print(type(x))
+print(type(y))
+print(type(z))
+ 
