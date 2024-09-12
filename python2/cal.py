fe70a3c441b928dc1a3851911f75d5cc16e1d64c
siddhant lokhande <lokhandesiddharth196@gmail.com>
2024-09-12 14:49:32 +0530

    include

---
+++ python2/cal.py
@@ -0,0 +1,13 @@
+a=45
+b=65
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+modulo=a%b
+
+print("the value of ",a,"+",b,"is =",add)
+print("the value of ",a,"-",b,"is =",sub)
+print("the value of ",a,"*",b,"is =",mul)
+print("the value of ",a,"/",b,"is =",div)
+print("the value of ",a,"%",b,"is =",modulo)
---
+++ python2/input.py
@@ -0,0 +1,8 @@
+a=input( "Enter the number1 :")
+b=input( "Enter the number2 :")
+
+print("the value of ",a,"+",b,"is =",int(a)+int(b))
+print("the value of ",a,"-",b,"is =",int(a)-int(b))
+print("the value of ",a,"*",b,"is =",int(a)*int(b))
+print("the value of ",a,"/",b,"is =",int(a)/int(b))
+print("the value of ",a,"%",b,"is =",int(a)%int(b))
