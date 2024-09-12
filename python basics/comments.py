49679f2881fcdbada23d136a678a125455a6e71e
siddhant lokhande <lokhandesiddharth196@gmail.com>
2024-09-12 13:43:02 +0530

    include

---
+++ python basics/comments.py
@@ -0,0 +1,15 @@
+print("hello im siddhant im learning the python ")
+
+# hello im siddhant im learning the python (single comment line )
+
+"""ChatGPT is a chatbot and virtual assistant developed by OpenAI and launched on November 30,
+2022. Based on large language models, it enables users to refine 
+and steer a conversation towards a desired length, format, style, level of detail, and language"""
+
+#(for multiline comment )
+
+'''ChatGPT is a chatbot and virtual assistant developed by OpenAI and launched on November 30, 
+2022. Based on large language models, it enables users to refine and
+steer a conversation towards a desired length, format, style, level of detail, and language'''
+
+#(for multiline comment )
